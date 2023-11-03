5d8d22b175d5111fd36c80ba99bf01568afad26b
M4K4TT4CK <121465111+M4K4TT4CK@users.noreply.github.com>
2023-11-03 18:19:24 -0400

    Finalize TimeZoneConversion and TimeZoneConversionController

--- src/main/java/edu/wgu/d387_sample_code/controller/TimeZoneConversion.java
+++ src/main/java/edu/wgu/d387_sample_code/controller/TimeZoneConversion.java
@@ -1,4 +1,24 @@
 package edu.wgu.d387_sample_code.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+@CrossOrigin(origins = "http://localhost:4200")
 public class TimeZoneConversion {
+
+    public static String getTime(){
+
+        // Create DateTimeObject
+        ZonedDateTime time = ZonedDateTime.now();
+        DateTimeFormatter theTimeFormat = DateTimeFormatter.ofPattern("HH:mm");
+
+        // Different time zone conversion
+        ZonedDateTime eT = time.withZoneSameInstant(ZoneId.of("America/New_York"));
+        ZonedDateTime mT = time.withZoneSameInstant(ZoneId.of("America/Denver"));
+        ZonedDateTime utc = time.withZoneSameInstant(ZoneId.of("UTC"));
+
+        // return time string output
+        return eT.format(theTimeFormat) + " EST, " + mT.format(theTimeFormat) + " MTN, " + utc.format(theTimeFormat) + " UTC";
+    }
 }
--- src/main/java/edu/wgu/d387_sample_code/controller/TimeZoneConversionController.java
+++ src/main/java/edu/wgu/d387_sample_code/controller/TimeZoneConversionController.java
@@ -1,4 +1,17 @@
 package edu.wgu.d387_sample_code.controller;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@CrossOrigin(origins = "http://localhost:4200")
 public class TimeZoneConversionController {
+
+    @GetMapping("/presentation")
+    public ResponseEntity<String> presentationDisplay(){
+        String display = "The presentation will begin at: ";
+        return new ResponseEntity<String> (display, HttpStatus.OK);
+    }
 }
